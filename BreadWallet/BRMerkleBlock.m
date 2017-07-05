4895caeacac202d823403aa3a001ba6fa3af671c
Jonathan <839560084@qq.com>
2017-07-05 12:12:54 +0800

    modify params for mbc

--- BreadWallet/BRMerkleBlock.m
+++ BreadWallet/BRMerkleBlock.m
@@ -29,7 +29,7 @@
 
 #define MAX_TIME_DRIFT    (2*60*60)     // the furthest in the future a block is allowed to be timestamped
 #define MAX_PROOF_OF_WORK 0x1d00ffffu   // highest value for difficulty target (higher values are less difficult)
-#define TARGET_TIMESPAN   (14*24*60*60) // the targeted timespan between difficulty target adjustments
+#define TARGET_TIMESPAN   (60 * 60 * 2) // the targeted timespan between difficulty target adjustments
 
 // from https://en.bitcoin.it/wiki/Protocol_specification#Merkle_Trees
 // Merkle trees are binary trees of hashes. Merkle trees in bitcoin use a double SHA-256, the SHA-256 hash of the
